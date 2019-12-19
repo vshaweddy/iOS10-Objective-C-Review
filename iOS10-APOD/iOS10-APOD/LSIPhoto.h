4478b48f6298368505c48158350a4972e09b2ffb
Paul Solt <PaulSolt@gmail.com>
2019-12-19 14:15:59 -0500

    Rearranging properties

--- iOS10-APOD/iOS10-APOD/LSIPhoto.h
+++ iOS10-APOD/iOS10-APOD/LSIPhoto.h
@@ -12,12 +12,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LSIPhoto : NSObject
 
-//@property NSString *copyright;
-//@property NSDate *date;
-//@property NSString *explanation;
-//@property NSURL *hdURL;
 @property NSString *title;
 @property NSURL *url;
+@property NSURL *hdURL;
+@property NSString *explanation;
+@property NSDate *date;
+@property NSString *copyright;
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
